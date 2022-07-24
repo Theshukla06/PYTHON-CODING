025ad5a831bbfe144061e6c3edc22ae68bf007df
Theshukla06 <en21ca501023@gmail.com>
2022-07-24 19:04:58 +0530

    Area Of Circle using Function

--- 07_area_of_circle.py
+++ 07_area_of_circle.py
@@ -1,2 +1,8 @@
-PI=3.14
-radius=
+from math import pi
+
+def Area_Of_Circle(r):
+    print('You Enter Value n is :-',n)
+    return pi*r*r
+
+n=int(input('Enter Any Number :-'))
+print(Area_Of_Circle(n))
