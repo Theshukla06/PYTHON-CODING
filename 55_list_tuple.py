fe13c1899b38c04a199156997177c1421bee5993
Theshukla06 <en21ca501023@gmail.com>
2022-09-10 15:18:30 +0530

    new work

---
+++ 55_list_tuple.py
@@ -0,0 +1,10 @@
+# list1= ['Ankit','Shukla','Arvind','Netsetos']
+# list1[1] = 'Pandit'
+# list1.remove('Ankit')
+# print(list1)
+
+tup1 = ('Ankit','Shukla','Arvind','Netsetos')
+tup2= ('Add',)
+
+tup3 = tup1+tup2
+print(tup3)
--- my_work.py
+++ my_work.py
@@ -1,92 +1,92 @@
-# # # # # # # # a=int(input('enter a'))
-# # # # # # # # b=int(input('enter b'))
-# # # # # # # # sum=a+b
-# # # # # # # # print(sum)
+# a=int(input('enter a'))
+# b=int(input('enter b'))
+# sum=a+b
+# print(sum)
 
-# # # # # # # def sum(a,b):
-# # # # # # #     return a+b
+# def sum(a,b):
+#     return a+b
 
-# # # # # # # a=int(input('enter a'))
-# # # # # # # b=int(input('enter b'))
+# a=int(input('enter a'))
+# b=int(input('enter b'))
 
-# # # # # # # a1=sum 
-# # # # # # # print(a1)
+# a1=sum 
+# print(a1)
 
-# # # # # # n=int(input("enter number"))
-# # # # # # for i in range(1,11):
-# # # # # #     print(n,"X",i,"=",n*i)
+# n=int(input("enter number"))
+# for i in range(1,11):
+#     print(n,"X",i,"=",n*i)
     
-# # # # # #     # table=n*i
-# # # # # #     # print("*"i="table")
-# # # # # num = int(input("Enetr a no :-"))
-# # # # # if num % 2 == 0:
-# # # # #     print('prime no')
+#     # table=n*i
+#     # print("*"i="table")
+# num = int(input("Enetr a no :-"))
+# if num % 2 == 0:
+#     print('prime no')
 
-# # # # # else:
-# # # # #     print('not prime no')
+# else:
+#     print('not prime no')
 
-# # # # # a="Shukla"
-# # # # # print(type(a))
-# # # # # n=int(a)
-# # # # # print(type(n))
+# a="Shukla"
+# print(type(a))
+# n=int(a)
+# print(type(n))
 
-# # # # # for i in range (1,10+1):
-# # # # #     print(i)
+# for i in range (1,10+1):
+#     print(i)
 
-# # # # # x='2'
+# x='2'
 
-# # # # # print(x)
-# # # # # x=int(x)
-# # # # # print(type(x))
+# print(x)
+# x=int(x)
+# print(type(x))
 
-# # # # # #BOOLEAN
-# # # # # a= 5
-# # # # # b= 10
-# # # # # c= a<b
-# # # # # print(c)
-# # # # def Sum(a,b):
-# # # #     print('value A is :-',a)
-# # # #     print('value B is :-',b)
-# # # #     return a + b
+# #BOOLEAN
+# a= 5
+# b= 10
+# c= a<b
+# print(c)
+# def Sum(a,b):
+#     print('value A is :-',a)
+#     print('value B is :-',b)
+#     return a + b
 
-# # # # x= int(input("Enter A Value :- "))
-# # # # y= int(input("Enter B Value :- "))
-# # # # s=Sum(x,y)
-# # # # print(s)
+# x= int(input("Enter A Value :- "))
+# y= int(input("Enter B Value :- "))
+# s=Sum(x,y)
+# print(s)
 
-# # # #EVEN OR ODD NO PROGRAM
-# # # def Even_Odd(a):
-# # #     if a%2==0:
-# # #         print("Your no is Even :-",a)
-# # #     else:
-# # #         print("Your no is Odd :-",a)
-# # # x=int(input('Enter Value A :-'))
-# # # y=Even_Odd(x)
+# #EVEN OR ODD NO PROGRAM
+# def Even_Odd(a):
+#     if a%2==0:
+#         print("Your no is Even :-",a)
+#     else:
+#         print("Your no is Odd :-",a)
+# x=int(input('Enter Value A :-'))
+# y=Even_Odd(x)
 
-# # # def Factorial(n):
-# # #     fact = 1
-# # #     for i in range(1,n+1):
-# # #         fact = fact * i
-# # #         return fact
-# # # a=int(input('Enter a :- '))
-# # # b=Factorial(a)
-# # # print('Your Factorial Ans is :-',b)
+# def Factorial(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact = fact * i
+#         return fact
+# a=int(input('Enter a :- '))
+# b=Factorial(a)
+# print('Your Factorial Ans is :-',b)
 
-# # from unicodedata import name
+# from unicodedata import name
 
 
-# # class Student:
-# #     def setdata(self):
-# #         self.name=input('Enter Your Name :-')
-# #         self.age=int(input('Enter Your Age :-'))
-# #         self.colour=input('Enter Your Color :-')
-# #     def display(self):
-# #         print('Name :- ',self.name)
-# #         print('Age :- ',self.age)
-# #         print('Colour :- ',self.colour)
-# # a=Student()
-# # a.setdata()
-# # a.display()
+# class Student:
+#     def setdata(self):
+#         self.name=input('Enter Your Name :-')
+#         self.age=int(input('Enter Your Age :-'))
+#         self.colour=input('Enter Your Color :-')
+#     def display(self):
+#         print('Name :- ',self.name)
+#         print('Age :- ',self.age)
+#         print('Colour :- ',self.colour)
+# a=Student()
+# a.setdata()
+# a.display()
 
 # l=[1,'x',22,'a']
 # print(l)
@@ -307,10 +307,28 @@
 # for i in range(num):
 #     print(Feb(i))
 
-num = input("Enter a no :- ")
-rev = num[::-1]
+# num = input("Enter a no :- ")
+# rev = num[::-1]
 
-if rev != num:
-    print(num,"Its Not Palindrome")
+# if rev != num:
+#     print(num,"Its Not Palindrome")
+# else:
+#     print(num,"Is Palindrome")
+
+# ARMSTRONG
+
+num = int(input("Enter a three no :- "))
+temp = num
+sum = 0 
+digit = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+    
+if num == sum :
+    print(num,"Is Armstrong Number")
 else:
-    print(num,"Is Palindrome")
+    print(num,"Is Not Armstrong Number")
+
