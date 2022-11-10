5dd59565ddce4171e7711b89c599264d6489e7c3
Theshukla06 <en21ca501023@gmail.com>
2022-11-10 22:57:40 +0530

    new

--- 04_even_odd.py
+++ 04_even_odd.py
@@ -1,5 +1,12 @@
-a=int(input("Enter a 1st number :- "))
-if a%2==0:
+# a=int(input("Enter a 1st number :- "))
+# if a%2==0:
+#     print("Your number is even")
+# else:
+#     print("Your number is odd")
+
+num = int(input("Enter no :- "))
+if num % 2 == 0:
     print("Your number is even")
 else:
     print("Your number is odd")
+    
